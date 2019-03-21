b9c96a69887b72e3512ff1e8ad0a573a4081479e
Camilo Lopez <camilino123@gmail.com>
2019-03-21 17:24:10 -0500

    Article, tag and tagarticle models fixed, routes organized, hamlets to forms createds,  hamlets used in all handlers

--- src/Handler/Article.hs
+++ src/Handler/Article.hs
@@ -7,137 +7,83 @@
 module Handler.Article where
 
 import Import
-import Data.Text()
-import Control.Applicative   ((<$>), (<*>))
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,)
+import Yesod.Form.Bootstrap3
+import Database.Persist.Sql
 
 -- Article form to create an article
-articleForm :: Maybe Article -> AForm Handler Article
-articleForm article = Article
-    <$> areq intField  "Article id" Nothing
-    <*> areq intField  "User id"    Nothing
-    <*> areq textField "Content"    Nothing
-    <*> areq textField "Title"      Nothing
-    <*> aopt intField  "Score"      Nothing
+articleForm :: AForm Handler Article
+articleForm = Article
+    <$> areq textField (bfs ("Title" :: Text))      Nothing
+    <*> areq textField (bfs ("Content" :: Text))    Nothing
+    <*> areq intField  (bfs ("User id" :: Text))    Nothing
+    <*> aopt intField  (bfs ("Score" :: Text))      Nothing
 
 getCreateArticleR :: Handler Html
 getCreateArticleR = do
-    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm $ articleForm Nothing
-    defaultLayout
-        [whamlet|
-            <p>Create a new Article
-            <form method=post action=@{CreateArticleR} enctype=#{enctype}>
-                ^{widget}
-                <button>Submit article
-        |]
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm articleForm
+    defaultLayout $ do
+        $(widgetFile "articles/create")
 
 postCreateArticleR :: Handler Html
 postCreateArticleR = do
-    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm $ articleForm Nothing
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm articleForm
     case res of
         FormSuccess article -> do
             _ <- runDB $ insert article
             redirect CreateArticleR
-        _ -> defaultLayout
-            [whamlet|
-                <p>Error creating the article, try again
-                <form method=post action=@{CreateArticleR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Submit article
-            |]
+        _ -> defaultLayout $ do
+            $(widgetFile "articles/create")
 
 -- Data to delete an article
 data DeleteArticle = DeleteArticle
     { delArticleId :: Int
-    , delUserId    :: Int
     } deriving (Show, Eq)
 
--- Getter articleId from delete form
-getArticleId (DeleteArticle delArticleId _) = delArticleId
-
--- Getter userId from delete form
-getUserId (DeleteArticle _ delUserId) = delUserId
-
-deleteArticleForm :: Maybe DeleteArticle -> AForm Handler DeleteArticle
-deleteArticleForm deleteArticle = DeleteArticle
-    <$> areq intField "Article ID" Nothing
-    <*> areq intField "User ID" Nothing
+deleteArticleForm ::AForm Handler DeleteArticle
+deleteArticleForm = DeleteArticle
+    <$> areq intField (bfs ("Article ID" :: Text)) Nothing
 
 getDeleteArticleR :: Handler Html
 getDeleteArticleR = do
-    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm $ deleteArticleForm Nothing
-    defaultLayout
-        [whamlet|
-            <p>Here you can delete an article
-            <form method=post action=@{DeleteArticleR} enctype=#{enctype}>
-                ^{widget}
-                <button>Delete article
-        |]
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm deleteArticleForm
+    defaultLayout $ do
+        $(widgetFile "articles/delete")
 
 postDeleteArticleR :: Handler Html
 postDeleteArticleR = do
-    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm $ deleteArticleForm Nothing
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm deleteArticleForm
     case res of
         FormSuccess article -> do
-            _ <- runDB $ deleteWhere [ArticleArticleId ==. getArticleId article, ArticleUserId ==. getUserId article]
+            _ <- runDB $ deleteWhere [ArticleId ==. toSqlKey (fromIntegral (delArticleId article))]
             redirect CreateArticleR
-        _ -> defaultLayout
-            [whamlet|
-                <p>Error deleting the article, try again
-                <form method=post action=@{DeleteArticleR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Delete article
-            |]
+        _ -> defaultLayout $ do
+            $(widgetFile "articles/delete")
 
 -- Data to update an article
 data UpdateArticle = UpdateArticle
-    { updUserId    :: Int
-    , updArticleId :: Int
+    {updArticleId :: Int
     , title     :: Text
     , content   :: Text
     } deriving (Show, Eq)
 
--- Getter articleId from update form
-getArticleIdUpdate (UpdateArticle _ updArticleId _ _) = updArticleId
-
--- Getter userId from update form
-getUserIdUpdate (UpdateArticle updUserId _ _ _) = updUserId
-
--- Getter title from update form
-getTitleUpdate (UpdateArticle _ _ title _) = title
-
--- Getter content from update form
-getContentUpdate (UpdateArticle _ _ _ content) = content
-
-updateArticleForm :: Maybe UpdateArticle -> AForm Handler UpdateArticle
-updateArticleForm updateArticle = UpdateArticle
-    <$> areq intField  "User id"    Nothing
-    <*> areq intField  "Article id" Nothing
-    <*> areq textField "Title"      Nothing
-    <*> areq textField "Content"    Nothing
+updateArticleForm :: AForm Handler UpdateArticle
+updateArticleForm = UpdateArticle
+    <$> areq intField  (bfs ("Article id" :: Text)) Nothing
+    <*> areq textField (bfs ("Title" :: Text))      Nothing
+    <*> areq textField (bfs ("Content" :: Text))    Nothing
 
 getUpdateArticleR :: Handler Html
 getUpdateArticleR = do
-    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm $ updateArticleForm Nothing
-    defaultLayout
-        [whamlet|
-            <p>Here you can update an article
-            <form method=post action=@{UpdateArticleR} enctype=#{enctype}>
-                ^{widget}
-                <button>Update article
-        |]
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm updateArticleForm
+    defaultLayout $ do
+        $(widgetFile "articles/update")
 
 postUpdateArticleR :: Handler Html
 postUpdateArticleR = do
-    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm $ updateArticleForm Nothing
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm updateArticleForm
     case res of
         FormSuccess article -> do
-            _ <- runDB $ updateWhere [ArticleArticleId ==. getArticleIdUpdate article, ArticleUserId ==. getUserIdUpdate article] [ArticleTitle =. getTitleUpdate article, ArticleContent =. getContentUpdate article]
+            _ <- runDB $ updateWhere [ArticleId ==. toSqlKey (fromIntegral (updArticleId article))] [ArticleTitle =. title article, ArticleContent =. content article]
             redirect CreateArticleR
-        _ -> defaultLayout
-            [whamlet|
-                <p>Error updating the article, try again
-                <form method=post action=@{DeleteArticleR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Update article
-            |]
+        _ -> defaultLayout $ do
+                $(widgetFile "articles/update")
---
+++ src/Handler/User.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.User where
+
+import Import
+import Yesod.Form.Bootstrap3
+
+-- Data to search articles by tag
+data SearchArticleByTag = SearchArticleByTag
+    {tagName :: Text
+    } deriving (Eq)
+
+-- Getter tagName from search form
+getTagName (SearchArticleByTag tagName) = tagName
+
+searchArticleByTagForm :: AForm Handler SearchArticleByTag
+searchArticleByTagForm = SearchArticleByTag
+    <$> areq textField (bfs ("Tag name" :: Text)) Nothing
+
+getSearchArticleByTagR :: Handler Html
+getSearchArticleByTagR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm searchArticleByTagForm
+    defaultLayout $ do
+        $(widgetFile "articles/search")
+
+postSearchArticleByTagR :: Handler Html
+postSearchArticleByTagR = do
+    ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm searchArticleByTagForm
+    case res of
+        FormSuccess search -> do
+            tag <- runDB $ selectList [TagName ==. getTagName search] []
+            defaultLayout [whamlet|#{show tag}|]
+        _ -> defaultLayout $ do
+            $(widgetFile "articles/search")
