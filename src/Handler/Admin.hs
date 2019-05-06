3d046aa3a02ca1279fcf5c67985ded53ee5eb2bd
Camilo López <camilino123@gmail.com>
2019-05-06 16:51:51 -0500

    Admin handler created

---
+++ src/Handler/Admin.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Admin where
+
+import Import
+import Yesod.Form.Bootstrap3
+import Database.Persist.Sql
